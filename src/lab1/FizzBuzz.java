847bb10c10cb066df6facc6894a0d17d78a0cb1c
戴宝翼 <brad@daibaoyideMacBook-Air.local>
2025-09-08 12:51:42 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -8,14 +8,14 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i <= 100) {
+        while (i < 100) {
 
             // Find out which numbers divide i.
-            i = getI(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int getI(int i) {
+    public static int doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
